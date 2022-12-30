c8033a0e28a45f75ec009b10dcdaa72609a4a553
roy-seah <seah_ryo@yahoo.com.sg>
2022-12-31 01:05:09 +0800

    Allow origins changed to front end endpoint

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3002",
+		AllowOrigins: "http://localhost:3000",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
